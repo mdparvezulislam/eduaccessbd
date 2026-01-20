6149d8968c33f1c7422773e1162f3a881951e0f7
mdparvezulislam <codingloverparvez@gmail.com>
2026-01-20 14:23:22 +0600

    remote pattern add

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,27 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
-    domains: ["images.unsplash.com", "ik.imagekit.io","github.com"],
+    // domains: ["images.unsplash.com", "ik.imagekit.io","github.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
