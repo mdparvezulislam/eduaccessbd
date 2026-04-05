cdbfc9b5c59c4c1f8d85236b9219d7aff017be46
mdparvezulislam <codingloverparvez@gmail.com>
2026-04-05 21:48:04 +0600

    add docker

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-
+output: 'standalone',
   // 🔥 VERY IMPORTANT for VPS (low RAM)
   experimental: {
     workerThreads: false,
